44bdf78448908c089921a31d3abc74929f77f03f
jEllerGRC <eller.jared@student.greenriver.edu>
2023-06-01 13:26:35 -0700

    Added documentation and a little joke I found online to try to brighten the mood of anyone who looks at this project.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+/**
+ * This is the entry point to a sample app that instructs me on the basics of VCS.
+ *
+ * @author Jared Eller
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Starts the application and prints out some messages to the console.
+     * @param args command-line arguments (unused)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world, from SDEV 220!");
         System.out.println("Jared was here on June 1, 2023.");
         System.out.println("He was eating a bowl of cheerios and working on this during class. 🥣😋");
     }
+
+    /*
+    *I recently rejected a junior software dev job at IKEA.
+    *I kinda know java, kotlin and some php but unfortunately Assembly was required.
+    */
 }

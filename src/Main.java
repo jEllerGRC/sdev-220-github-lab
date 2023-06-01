b6821bd4b18f4f2d590dd7ffc694362df118fd32
jEllerGRC <eller.jared@student.greenriver.edu>
2023-06-01 13:14:23 -0700

    Committing driver file. Includes a few short Strings describing the situation in which this project was created.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world, from SDEV 220!");
+        System.out.println("Jared was here on June 1, 2023.");
+        System.out.println("He was eating a bowl of cheerios and working on this during class. 🥣😋");
+    }
+}
